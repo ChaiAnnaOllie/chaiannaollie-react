268c8c2689fe77aa14388bb1aa6ef0d9ebf542cd
ChaiAnnaOllie <veekas46@gmail.com>
2025-03-28 14:27:23 -0400

    Initial commit - final clean setup

---
+++ chaiannaollie-react-final/src/pages/Testimonials.js
@@ -0,0 +1,9 @@
+
+export default function Testimonials() {
+  return (
+    <div>
+      <h1>What People Are Saying</h1>
+      <p>Read testimonials from our amazing community of dog lovers who have used our products and services!</p>
+    </div>
+  );
+}
