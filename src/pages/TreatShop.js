af65cfddce95fb56396aba839e050e1e96d03fdb
ChaiAnnaOllie <veekas46@gmail.com>
2025-03-30 22:44:23 -0400

    treat shop

---
+++ src/pages/TreatShop.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function TreatShop() {
+  return (
+    <div>
+      <h1>Welcome to the Treat Shop</h1>
+      <p>This is a placeholder page for the Treat Shop. More treats coming soon!</p>
+    </div>
+  );
+}
+
+export default TreatShop;
