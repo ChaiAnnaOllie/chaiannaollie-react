2dba7ed0c4a5c722d47737fce19b60efb3b162b3
ChaiAnnaOllie <veekas46@gmail.com>
2025-03-30 22:59:31 -0400

    recipe library

---
+++ src/pages/RecipeLibrary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function RecipeLibrary() {
+  return (
+    <div>
+      <h1>Recipe Library</h1>
+      <p>This section is available to members only. Purchase access to view exclusive recipes and videos.</p>
+      <p>Login to your account to comment and interact with the community.</p>
+      <p><em>Coming soon: secure member login and commenting features.</em></p>
+    </div>
+  );
+}
+
+export default RecipeLibrary;
