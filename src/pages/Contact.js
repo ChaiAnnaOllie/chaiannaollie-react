1bb398af4dffedd117e128eb3c3b1bf7f7657257
ChaiAnnaOllie <veekas46@gmail.com>
2025-03-30 20:13:34 -0400

    added contacts

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>This is a placeholder contact page. You can add your contact form or info here later.</p>
+    </div>
+  );
+}
+
+export default Contact;
