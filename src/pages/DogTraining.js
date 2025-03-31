e056129ad3389fffb6ce12b7c9f3bb5ee30082b8
ChaiAnnaOllie <veekas46@gmail.com>
2025-03-30 20:20:56 -0400

    dog training page

---
+++ src/pages/DogTraining.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DogTraining = () => {
+  return (
+    <div>
+      <h1>Dog Training</h1>
+      <p>This page is coming soon!</p>
+    </div>
+  );
+};
+
+export default DogTraining;
