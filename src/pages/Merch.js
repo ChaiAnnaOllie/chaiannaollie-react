923ac263cfd9e7b01c37518d9abee133a21ecb60
ChaiAnnaOllie <veekas46@gmail.com>
2025-03-31 11:32:47 -0400

    merch page

---
+++ src/pages/Merch.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+function Merch() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>ChaiAnnaOllie Merch</h1>
+      <p>Explore our exclusive collection of merch!</p>
+      <p>
+        This page will be connected to our Printify store. 
+        You’ll be able to browse and shop for custom items like apparel, mugs, accessories, and more.
+      </p>
+      <p>
+        Check back soon for the full catalog, or follow us on social media for updates!
+      </p>
+      {/* Future: Embed Printify product feed or redirect buttons */}
+    </div>
+  );
+}
+
+export default Merch;
